1e900edf2f03338b5ec7af308f91891a0ea47fe6
J Fixby <github@jfixby.com>
2025-04-08 12:47:26 -0300

    add cmd args

--- client/main.go
+++ client/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 4; i++ {
 		Exchange("localhost:1337")
 	}
 }
--- server/main.go
+++ server/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jfixby/tcptest/server/server"
+	"os"
 )
 
 func main() {
-	s := server.NewServer()
+	// Проверяем наличие аргументов: адрес и путь к файлу с мудростями
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run main.go <address> <wisdoms_file>")
+		os.Exit(1)
+	}
 
-	err := s.Start(":1337", "wisdoms.json")
-	if err != nil {
-		panic(err)
+	address := os.Args[1]     // Пример: ":1337"
+	wisdomsFile := os.Args[2] // Пример: "server/wisdoms.json"
+
+	s := server.NewServer()
+	if err := s.Start(address, wisdomsFile); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
 	}
 }
